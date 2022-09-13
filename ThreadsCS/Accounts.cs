f48641abf92f5db97e93d2905784b9a4662b8885
sahithi.annavarapu@bhavnacorp.com <sahithilakshmi9898@gmail.com>
2022-09-13 17:45:07 +0530

    Day31 Session1 Threads

---
+++ ThreadsCS/Accounts.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ThreadsCS
+{
+    class Accounts
+    {
+        static void Main(string[] args)
+        {
+            Transactions tc = new Transactions(10000);         
+            Update(tc);
+        }
+        static void Update(Transactions tc)
+        {
+         Console.WriteLine("enter y/n to continue");
+            string repeat = Console.ReadLine();
+            while (repeat == "y")
+            {
+                Console.WriteLine("1:credit");
+                Console.WriteLine("2:debit");
+                Console.WriteLine("3:transactions");
+
+            choice: Console.WriteLine("enter the choice");
+                int n = int.Parse(Console.ReadLine());
+
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine("ENTER THE CREDITED AMOUNT: ");
+                        decimal amt = Convert.ToDecimal(Console.ReadLine());
+                        // Thread t = new Thread(new ThreadStart(tc.CreditAmt(amt)));
+                        // t.Start();
+                        tc.CreditAmt(amt);
+                        break;
+                    case 2:
+                        Console.WriteLine("ENTER THE debited AMOUNT: ");
+                        decimal amt1 = Convert.ToDecimal(Console.ReadLine());
+                        tc.DebitAmt(amt1);
+                        break;
+                    case 3:
+                        tc.GetTransactions();
+                        break;
+                    default:
+                        Console.WriteLine("invalid options");
+                        goto choice;
+                }
+                Console.WriteLine("to continue-y/n-exit");
+                 repeat = Console.ReadLine();
+            }
+        }
+    }
+    public class Transactions
+    {
+        private readonly object lockbalance = new object();
+        private decimal balance;
+        public Transactions(decimal initialBal) => balance = initialBal;
+        List<decimal> total = new List<decimal>();
+        public void CreditAmt(decimal amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "the creadited amount cant be negetive");
+            }
+            lock (lockbalance)
+            {
+                balance += amount;
+                Console.WriteLine("balance=" + balance);
+                total.Add(balance);
+                Console.WriteLine("transaction of credied amount:" + total[1]);
+            }
+        }
+        public decimal DebitAmt(decimal amount)
+        {
+            if(amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "The debited amount can't be negetive");
+            }
+            decimal appliedamt = 0;
+            lock (lockbalance)
+            {
+                if(balance >= amount)
+                {
+                    balance -= amount;
+                    appliedamt = amount;
+                    Console.WriteLine("balance"+balance);
+                    total.Add(balance);
+                    Console.WriteLine("transaction of debited amount:"+total[1]);
+                }
+            }
+            return appliedamt;
+        }
+     
+        public List<decimal> GetTransactions()
+        {
+            lock (lockbalance)
+            {
+
+                for (int i = 0; i < total.Count;i++)
+                {
+                    Console.WriteLine("Transaction: "+total[i]);
+                }
+            }
+            return total;
+        }
+    }
+
+}
---
+++ ThreadsCS/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadsCS
+{
+
+    class Program
+    {
+      // Custom delegate- public delegate void ThreadDel();
+        static void Main(string[] args)
+        {
+
+             Program pr = new Program();
+            Console.WriteLine("thread started");
+         
+            //using parameterized delagate
+             Thread thread = new Thread(new ParameterizedThreadStart(Message));
+            //using delegate
+            Thread thread2 = new Thread(new ThreadStart(Threadclass));
+            //passing message to func
+            Thread thread3 = new Thread((message) => Console.WriteLine(message));
+
+            //custom delegate
+            // Thread thread3 = new Thread(new ThreadDel(Threadclass));
+            thread.Start("thread1 is running");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"Main Thread: {i}");
+                //Pause thread
+                Thread.Sleep(300);
+            }
+            thread2.Start();
+            thread3.Start("thread3 is running");
+
+            for(int x = 1; x < 5; x++)
+            {
+                Thread thread4 = new Thread(Print);
+                thread4.Name = $"thread {x}";
+                thread4.Start();
+            }
+
+        }
+        static void Print()
+        {
+            int y = 1;
+            object locker = new object();
+            lock (locker) 
+            {
+                for (int x = 1; x < 5; x++)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name}:{y}");
+                    y++;
+                    Thread.Sleep(100);
+                }
+            }
+           
+            //    Console.WriteLine(locker + "\n");
+            
+        }
+        //Console.ReadLine();
+        static void Threadclass()
+        {
+            Console.WriteLine("second thread");
+            Thread t1 = Thread.CurrentThread;
+            t1.Name = "thread1";
+            Console.WriteLine($"thread name: {t1.Name}");
+            Console.WriteLine($"thread managethread: {t1.ManagedThreadId}");
+            Console.WriteLine($"thread culture: {t1.CurrentCulture}");
+            Console.WriteLine($"thread execution context: {t1.ExecutionContext}");
+            Console.WriteLine($"thread priority: {t1.Priority}");
+            Console.WriteLine($"thread background: {t1.IsBackground}");
+            Console.WriteLine("thread ending");
+            Thread.Sleep(1000);
+        }
+        static void Message(object message)
+        {
+            Console.WriteLine("message: " + message);
+        }
+
+    }
+}
