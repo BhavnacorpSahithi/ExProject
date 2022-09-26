6cb3298a7d6b2ff3e383b5c34e17a74f22aee52a
sahithi.annavarapu@bhavnacorp.com <sahithilakshmi9898@gmail.com>
2022-09-26 18:14:53 +0530

    Assessment8 MVC Shopping cart

---
+++ Assessment8MVC/Controllers/ExampleController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleMVC.Controllers
+{
+    public class ExampleController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
